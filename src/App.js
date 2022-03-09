000914311de2bf69158ad6c01141b48f2dc5bd7d
unknown <ag.macazz@gmail.com>
2022-03-09 10:52:46 -0300

    API working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const ApiKey = "0cdc104e20294b5e9931a1c0eaa2f126";
+  const exampleReq =
+  "https://api.spoonacular.com/food/menuItems/search?apiKey=0cdc104e20294b5e9931a1c0eaa2f126&query=vegan&number=4"
+  useEffect(() => {
+    getMenus();
+  }, []);
+  const getMenus = async () => {
+    const response = await fetch(exampleReq);
+    const data = await response.json();
+    console.log(data);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input></input>
+      <button>search</button>
     </div>
   );
 }
