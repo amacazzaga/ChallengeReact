de46c6e263677a0e0844c38833c29e0632167d45
unknown <ag.macazz@gmail.com>
2022-04-26 12:29:19 -0300

    class form-control, both inputs

--- src/Form.js
+++ src/Form.js
@@ -1,23 +1,36 @@
-import React from 'react'
+import React from "react";
 
 const Form = () => {
   return (
     <div>
-        <div class="mb-3 row">
-    <label for="staticEmail" class="col-sm-2 col-form-label">Email</label>
-    <div class="col-sm-10">
-      <input type="text" readonly class="form-control-plaintext" id="staticEmail" value="email@example.com"></input>
+      <div class="mb-3 row">
+        <label for="staticEmail" class="col-sm-2 col-form-label">
+          Email
+        </label>
+        <div class="col-sm-10">
+          <input
+            type="text"
+            class="form-control"
+            name="staticEmail"
+            value=""
+          ></input>
+        </div>
+      </div>
+      <div class="mb-3 row">
+        <label for="inputPassword" class="col-sm-2 col-form-label">
+          Password
+        </label>
+        <div class="col-sm-10">
+          <input
+            type="password"
+            class="form-control"
+            name="inputPassword"
+          ></input>
+        </div>
+        <button>SEND!</button>
+      </div>
     </div>
-  </div>
-  <div class="mb-3 row">
-    <label for="inputPassword" class="col-sm-2 col-form-label">Password</label>
-    <div class="col-sm-10">
-      <input type="password" class="form-control" id="inputPassword"></input>
-    </div>
-    <button>SEND!</button>
-  </div>
-    </div>
-  )
-}
+  );
+};
 
-export default Form
+export default Form;
