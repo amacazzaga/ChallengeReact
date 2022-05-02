650cc48c50a9a0e1dbc1b2213b91ef6d986515d6
unknown <ag.macazz@gmail.com>
2022-05-02 14:54:53 -0300

    conditional rendering on form and main

--- src/App.js
+++ src/App.js
@@ -17,17 +17,18 @@ function App() {
   const [choosedDishes, setChoosedDishes] = useState([]);
   const [isLoggedIn, setLoggedIn] = useState(false);
   ///////////////////////////
+  useEffect(() => {
+    getMenus(baseUrl);
+  }, []); // lo primero que aparece en la web
+  ////////////////////////////////////////
   const getMenus = async (url) => {
+    //getMenus tiene un endpoint q depende del parametro dado
     const response = await fetch(url);
     const data = await response.json();
     setMenu(data.menuItems);
     SetLoading(false);
   };
   //////////////////////////////
-   useEffect(() => {
-    getMenus(baseUrl);
-  }, []); // lo primero que aparece en la web
-  ////////////////////////////////////////
   const onAuthSuccess = (token) => {
     localStorage.setItem("token", token);
     setLoggedIn(true);
@@ -36,9 +37,7 @@ function App() {
   /*use effect con el item del token
   if() , render piola
   */
-////////////////////////////////////
- 
-/////////////////////////////////////////
+  /////////////////////////////////////////
   const addChoosedDish = async (id) => {
     //tomo como param id, que dp se carga de valor al hacer click en MenuItem
     const url = `https://api.spoonacular.com/food/menuItems/${id}?apiKey=${ApiKey}`;
@@ -46,86 +45,91 @@ function App() {
     const data = await response.json();
     setChoosedDishes(choosedDishes.concat(data));
   };
-////////////////////////////////////////////
+  ////////////////////////////////////////////
   return (
     <div>
       <div className="container-form">
-        <Form onAuthSuccess={onAuthSuccess} />
-        <div>{isLoggedIn ? "logeado" : "no logeado"}</div>
-      </div>
-      <div>
-        <main className="container">
-          <div className="modal-login">
-            <h1>MENU FROM HOTEL</h1>
-            <ButtonLoggedOut onClick={()=>{
-              localStorage.removeItem("token")
-              setLoggedIn(false)
-            }}/>
-            <div className="container-input-button">
-              <div className="input-div">
-                <input
-                  onChange={(e) => {
-                    SetInput(e.target.value);
-                  }}
-                ></input>
-              </div>
-              <div className="button-search-div">
-                <ButtonSearch
+        <div>
+          {!isLoggedIn ? (
+            <Form onAuthSuccess={onAuthSuccess} />
+          ) : (
+            <main className="container">
+              <div className="modal-login">
+                <h1>MENU FROM HOTEL</h1>
+                <ButtonLoggedOut
                   onClick={() => {
-                    const url = `https://api.spoonacular.com/food/menuItems/search?apiKey=${ApiKey}&query=${input}&${numberUrl}`;
-                    SetLoading(true);
-                    getMenus(url);
+                    localStorage.removeItem("token");
+                    setLoggedIn(false);
                   }}
                 />
-              </div>
-            </div>
-            <div className="container-md">
-              {menu.length > 0
-                ? menu.map((m) => (
-                    <MenuItem
-                      click={() => {
-                        addChoosedDish(m.id); //llamo a getdish con otros params:
-                        //aca le doy el id q va a url (el id, es el id de m, en este caso)
-                        //esta cargada en menuItem pero es lo q hace button choose
-                        console.log(choosedDishes);
+                <div className="container-input-button">
+                  <div className="input-div">
+                    <input
+                      onChange={(e) => {
+                        SetInput(e.target.value);
                       }}
-                      key={m.id}
-                      title={m.title}
-                      image={m.image}
-                      resto={m.restaurantChain}
+                    ></input>
+                  </div>
+                  <div className="button-search-div">
+                    <ButtonSearch
+                      onClick={() => {
+                        const url = `https://api.spoonacular.com/food/menuItems/search?apiKey=${ApiKey}&query=${input}&${numberUrl}`;
+                        SetLoading(true);
+                        getMenus(url);
+                      }}
                     />
-                  ))
-                : loading
-                ? "loading"
-                : "no hay"}
-            </div>
-          </div>
-          <div className="container-lg">
-            <h2>Your Selection:</h2>
-            {choosedDishes.map((d) => (
-              <ChoosedItem
-                click={() => {
-                  setChoosedDishes(
-                    choosedDishes.filter((dish) => {
-                      return dish.id != d.id;
-                    })
-                  );
-                }}
-                key={d.id}
-                title={d.title}
-                image={d.image}
-                calories={d.nutrition.calories}
-                score={d.spoonacularScore}
-              />
-            ))}
-          </div>
-          <script
-            src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-            integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-            crossorigin="anonymous"
-          ></script>
-        </main>
+                  </div>
+                </div>
+                <div className="container-md">
+                  {menu.length > 0 //se menu es mayor a cero, renderiza cada item
+                    ? menu.map((m) => (
+                        <MenuItem
+                          click={() => {
+                            addChoosedDish(m.id); //llamo a getdish con otros params:
+                            //aca le doy el id q va a url (el id, es el id de m, en este caso)
+                            //esta cargada en menuItem pero es lo q hace button choose
+                            console.log(choosedDishes);
+                          }}
+                          key={m.id}
+                          title={m.title}
+                          image={m.image}
+                          resto={m.restaurantChain}
+                        />
+                      ))
+                    : loading
+                    ? "loading"
+                    : "no hay"}
+                </div>
+              </div>
+              <div className="container-lg">
+                <h2>Your Selection:</h2>
+                {choosedDishes.map((d) => (
+                  <ChoosedItem
+                    click={() => {
+                      setChoosedDishes(
+                        choosedDishes.filter((dish) => {
+                          return dish.id != d.id;
+                        })
+                      );
+                    }}
+                    key={d.id}
+                    title={d.title}
+                    image={d.image}
+                    calories={d.nutrition.calories}
+                    score={d.spoonacularScore}
+                  />
+                ))}
+              </div>
+              <script
+                src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+                integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+                crossorigin="anonymous"
+              ></script>
+            </main>
+          )}
+        </div>
       </div>
+      <div></div>
     </div>
   );
 }
